713f287a33864a1a390903c666f673a5ddcd6320
Gibran Hemani <explodecomputer@gmail.com>
2018-03-09 02:56:35 +0000

    updated

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -10,13 +10,13 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+# PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+PASS="Odeadfsws123qwe"
 _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating MySQL admin user with ${_word} password"
 
 mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
-
 
 echo "=> Done!"
 
@@ -28,5 +28,12 @@ echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "MySQL user 'root' has no password but only allows local connections"
 echo "========================================================================"
+
+
+wget -O explodecomputer.sql "https://www.dropbox.com/s/lknzahljfqo2q44/explodecomputer.sql?dl=1"
+
+
+echo "DROP DATABASE IF EXISTS explodecomputer; CREATE DATABASE explodecomputer; GRANT ALL PRIVILEGES ON explodecomputer.* To 'gib'@'localhost' IDENTIFIED BY 'Odeadfsws123qwe';" | mysql -u root -p
+mysql -u root -p explodecomputer < explodecomputer.sql
 
 mysqladmin -uroot shutdown
